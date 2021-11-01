5fc3260ae9dd46eb188a8be0ed31c4a6c2bec3a0
Shotaro Yamazaki <yamazaki.s0527@gmail.com>
2021-11-01 13:37:21 +0900

    指数オーダーのfibo関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function fibo(n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    return fibo(n-1) + fibo(n-2)
+}
+
+const length = 40
+for(let i = 0;i < length + 1; i++) {
+    console.log(fibo(i))
+}
