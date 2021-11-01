3a3cb3b152525cae8f7a17f12a80adb1a32312d6
Shotaro Yamazaki <yamazaki.s0527@gmail.com>
2021-11-01 13:44:49 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 'use strict';
 
+const memo = new Map()
+
+memo.set(0, 0)
+memo.set(1, 1)
+
 function fibo(n) {
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+    if(memo.has(n)) return memo.get(n)
+    const value = fibo(n-2) + fibo(n-1)
+    memo.set(n, value)
+    return value
 
-    return fibo(n-1) + fibo(n-2)
 }
-
 const length = 40
-for(let i = 0;i < length + 1; i++) {
-    console.log(fibo(i))
+
+for(let i = 0; i < length + 1; i++) {
+    console.log(fibo(i));
 }
